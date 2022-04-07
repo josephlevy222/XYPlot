08b3bb9384458919001099ecf3338e3445e16802
Joseph Levy <josephlevy222@gmail.com>
2022-04-07 09:07:08 -0400

    with target dependency

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "XYPlot",
-            dependencies: []),
+            dependencies: [.package(url: "https://github.com/josephlevy222/Utilities.git", branch: "main"),
+                           .package(url: "https://github.com/josephlevy222/NumericTextField.git", branch: "main")]),
         .testTarget(
             name: "XYPlotTests",
             dependencies: ["XYPlot"]),
