2272d542184b9f831cf7c682472697a0b5d9bdd7
Joseph Levy <josephlevy222@gmail.com>
2022-04-27 14:52:40 -0700

    Fixed dependencies

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "XYPlot",
-            dependencies: []),
+            dependencies: ["Utilities","NumericTextField"]),
         .testTarget(
             name: "XYPlotTests",
             dependencies: ["XYPlot"]),
