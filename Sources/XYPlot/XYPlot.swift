35b718318ee511ec74ad14d22876dfd727151cfd
Joseph Levy <josephlevy222@gmail.com>
2025-01-20 09:39:35 -0800

    Removed init in XYPlot

--- Sources/XYPlot/XYPlot.swift
+++ Sources/XYPlot/XYPlot.swift
@@ -72,14 +72,14 @@ public struct XYPlotTitle: View {
 }
 /// XYPlot is a view that creates an XYPlot of PlotData with optional
 public struct XYPlot: View {
-	public init(data: Binding<PlotData>) {
-		self._data = data
-		print("-", terminator: "")  // the following make endless loop or close to it?
-		print(data.wrappedValue.settings.title)
-		if let axis = data.wrappedValue.settings.xAxis { print( axis.title) }
-		if let axis = data.wrappedValue.settings.yAxis { print( axis.title) }
-		if let axis = data.wrappedValue.settings.sAxis { print( axis.title) }
-	}
+//	public init(data: Binding<PlotData>) {
+//		self._data = data
+//		print("-", terminator: "")  // the following make endless loop or close to it?
+//		print(data.wrappedValue.settings.title)
+//		if let axis = data.wrappedValue.settings.xAxis { print( axis.title) }
+//		if let axis = data.wrappedValue.settings.yAxis { print( axis.title) }
+//		if let axis = data.wrappedValue.settings.sAxis { print( axis.title) }
+//	}
 	
 	@Binding public var data : PlotData
 	
