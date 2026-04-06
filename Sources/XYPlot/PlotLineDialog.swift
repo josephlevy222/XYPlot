296eaa89695f00a2a58dc59fb6f8c61c0bf53cbd
josephlevy222 <josephlevy222@gmail.com>
2026-04-06 10:32:08 -0500

    ScrollView add back and list scroll disabled in PlotLineDialog

--- Sources/XYPlot/PlotLineDialog.swift
+++ Sources/XYPlot/PlotLineDialog.swift
@@ -40,126 +40,128 @@ public struct PlotLineDialog: View {
 	@State private var savedPointColor: Color = .red
 	@State private var useSecondary = false
 	public var body: some View {
-		List {
-			HStack {
-				Text("\(legend)"); Spacer()
-				ZStack(alignment: .center) { // line sample
-					Path { path in path.move(to: .zero); path.addLine(to: CGPoint(x: 100, y: 0))}
-						.stroke(lineColor, style: StrokeStyle(lineWidth: lineWidth, dash: lineStyles[lineStyle] ))
+		ScrollView {
+			List {
+				HStack {
+					Text("\(legend)"); Spacer()
+					ZStack(alignment: .center) { // line sample
+						Path { path in path.move(to: .zero); path.addLine(to: CGPoint(x: 100, y: 0))}
+							.stroke(lineColor, style: StrokeStyle(lineWidth: lineWidth, dash: lineStyles[lineStyle] ))
 #if os(iOS) && !targetEnvironment(macCatalyst)
-						.frame(width: 100, height: 0.5) // height 0.5 makes the line centered on the points !?
+							.frame(width: 100, height: 0.5) // height 0.5 makes the line centered on the points !?
 #else
-						.frame(width: 100, height: 1.0)
+							.frame(width: 100, height: 1.0)
 #endif
-					PointShapeView(shape: PointShape( {symbolShapes[pointStyle].path(in: $0)}, angle: symbolShapes[pointStyle].angle, fill: pointFill, color: pointColor, size: pointSize))
-						.offset(x: -25.0, y: 0)
-					PointShapeView(shape: PointShape( {symbolShapes[pointStyle].path(in: $0)}, angle: symbolShapes[pointStyle].angle, fill: pointFill, color: pointColor, size: pointSize))
-						.offset(x:  25.0, y: 0)
-				}}
-			HStack { Text("Use with right axis"); Spacer(); CheckBoxView(checked: $useSecondary)}
-			Section {
-				HStack {
-					Text("Line Name")
-					TextField("Name", text: $lineName)
-						.background(Color(.secondarySystemBackground)).border(Color.primary)
-				}
-				HStack(spacing: 0) {
-					ColorPicker("Line Color     ",selection: $lineColor).disabled(lineOff)
-#if !targetEnvironment(macCatalyst)
-						.fixedSize()
-#endif
-					Spacer()
-					Text(" Off ")
-					CheckBoxView(checked: $lineOff).onChange(of: lineOff) { off in
-						if off && lineColor != .clear { savedLineColor = lineColor }
-						lineColor = off ? .clear : savedLineColor
+						PointShapeView(shape: PointShape( {symbolShapes[pointStyle].path(in: $0)}, angle: symbolShapes[pointStyle].angle, fill: pointFill, color: pointColor, size: pointSize))
+							.offset(x: -25.0, y: 0)
+						PointShapeView(shape: PointShape( {symbolShapes[pointStyle].path(in: $0)}, angle: symbolShapes[pointStyle].angle, fill: pointFill, color: pointColor, size: pointSize))
+							.offset(x:  25.0, y: 0)
+					}}
+				HStack { Text("Use with right axis"); Spacer(); CheckBoxView(checked: $useSecondary)}
+				Section {
+					HStack {
+						Text("Line Name")
+						TextField("Name", text: $lineName)
+							.background(Color(.secondarySystemBackground)).border(Color.primary)
 					}
-				}
-				Stepper(value: $lineWidth, in: 0.0...10.0, step: 0.5 ) {
-					Text("Line Width: \(String(format: "%.1f", lineWidth))")
-						.onChange(of: lineWidth) { newWidth in
-							if newWidth == 0.0 { lineOff = true }
-						}
-				}
-				HStack {
-					Text("Line Style")
-					Spacer()
-					Dropdown(placeHolder: "Dash" , selection: $lineStyle, options: lineStyleNames)
-				}
-			}
-			Section {
-				HStack(spacing: 0) {
-					ColorPicker("Symbol Color",selection: $pointColor).disabled(pointOff)
+					HStack(spacing: 0) {
+						ColorPicker("Line Color     ",selection: $lineColor).disabled(lineOff)
 #if !targetEnvironment(macCatalyst)
-						.fixedSize()
+							.fixedSize()
 #endif
-					Spacer()
-					Text(" Off ")
-					CheckBoxView(checked: $pointOff).onChange(of: pointOff) { off in
-						if off && pointColor != .clear { savedPointColor = pointColor}
-						pointColor = off ? .clear : savedPointColor
-					}
-				}
-				
-				HStack { Text("Symbol Filled");Spacer(); CheckBoxView(checked: $pointFill)}
-				HStack {
-					Text("Symbol Shape")
-					Spacer()
-					Button(action:  { showDropdown = true }) {
-						if(pointColor == Color.clear) { Text("None")}
-						else {
-							PointShapeView(shape: PointShape(symbolShapes[pointStyle].shapePath.path,
-															 angle: symbolShapes[pointStyle].angle,
-															 fill: pointFill, color: pointColor, size: pointSize))
+						Spacer()
+						Text(" Off ")
+						CheckBoxView(checked: $lineOff).onChange(of: lineOff) { off in
+							if off && lineColor != .clear { savedLineColor = lineColor }
+							lineColor = off ? .clear : savedLineColor
 						}
 					}
-					.popover(isPresented: $showDropdown, attachmentAnchor: .rect(.bounds), arrowEdge: .bottom) {
-						VStack(spacing: 0) {
-							ForEach(symbolShapes.indices, id:\.self) { i in
-								VStack {
-									Button(action: {
-										showDropdown = false
-										pointStyle = i
-									}) {
-										PointShapeView(shape: PointShape(symbolShapes[i].path,
-																		 angle: symbolShapes[i].angle))
-										.padding().contentShape(Rectangle()) }
-									.background(Color(.systemBackground))
-									Divider()
+					Stepper(value: $lineWidth, in: 0.0...10.0, step: 0.5 ) {
+						Text("Line Width: \(String(format: "%.1f", lineWidth))")
+							.onChange(of: lineWidth) { newWidth in
+								if newWidth == 0.0 { lineOff = true }
+							}
+					}
+					HStack {
+						Text("Line Style")
+						Spacer()
+						Dropdown(placeHolder: "Dash" , selection: $lineStyle, options: lineStyleNames)
+					}
+				}
+				Section {
+					HStack(spacing: 0) {
+						ColorPicker("Symbol Color",selection: $pointColor).disabled(pointOff)
+#if !targetEnvironment(macCatalyst)
+							.fixedSize()
+#endif
+						Spacer()
+						Text(" Off ")
+						CheckBoxView(checked: $pointOff).onChange(of: pointOff) { off in
+							if off && pointColor != .clear { savedPointColor = pointColor}
+							pointColor = off ? .clear : savedPointColor
+						}
+					}
+					
+					HStack { Text("Symbol Filled");Spacer(); CheckBoxView(checked: $pointFill)}
+					HStack {
+						Text("Symbol Shape")
+						Spacer()
+						Button(action:  { showDropdown = true }) {
+							if(pointColor == Color.clear) { Text("None")}
+							else {
+								PointShapeView(shape: PointShape(symbolShapes[pointStyle].shapePath.path,
+																 angle: symbolShapes[pointStyle].angle,
+																 fill: pointFill, color: pointColor, size: pointSize))
+							}
+						}
+						.popover(isPresented: $showDropdown, attachmentAnchor: .rect(.bounds), arrowEdge: .bottom) {
+							VStack(spacing: 0) {
+								ForEach(symbolShapes.indices, id:\.self) { i in
+									VStack {
+										Button(action: {
+											showDropdown = false
+											pointStyle = i
+										}) {
+											PointShapeView(shape: PointShape(symbolShapes[i].path,
+																			 angle: symbolShapes[i].angle))
+											.padding().contentShape(Rectangle()) }
+										.background(Color(.systemBackground))
+										Divider()
+									}
+									.textFieldStyle(.automatic)
+									.buttonStyle(.plain)
 								}
-								.textFieldStyle(.automatic)
-								.buttonStyle(.plain)
 							}
 						}
 					}
+					Stepper(value: $pointSize, in: 0.2...3.0, step: 0.2 ) {
+						Text("Symbol Size: \(String(format: "%.2f", pointSize))").fixedSize()
+					}
 				}
-				Stepper(value: $pointSize, in: 0.2...3.0, step: 0.2 ) {
-					Text("Symbol Size: \(String(format: "%.2f", pointSize))").fixedSize()
-				}
-			}
-			//} old end of List
-			HStack {
-				Button("Cancel") { dismiss() }.foregroundColor(.accentColor)
-				Spacer()
-				Button(action: {
-					var plotLine = PlotLine()
-					plotLine.lineColor = lineColor
-					plotLine.lineStyle = StrokeStyle(lineWidth: lineWidth, dash: lineStyles[lineStyle])
-					let shape = symbolShapes[pointStyle]
-					plotLine.pointShape = PointShape(shape.path, angle: shape.angle, fill: pointFill,
-													 color: pointColor, size: pointSize )
-					plotLine.legend = lineName
-					plotLine.secondary = useSecondary
-					plotLine.values = plotData.plotLines[i].values
-					
-					plotData.plotLines[i] = plotLine
-					plotData.scaleAxes()
-					dismiss()
-				}) { Text("Ok").foregroundColor(.accentColor).contentShape(Rectangle())}
-			}.buttonStyle(RoundedCorners(color: Color.primary.opacity(0.1), shadow: 2 ))
-		} // new end of List
-		.background(Color(.systemBackground))
-		.frame(width: 300, height: 720)
+				HStack {
+					Button("Cancel") { dismiss() }.foregroundColor(.accentColor)
+					Spacer()
+					Button(action: {
+						var plotLine = PlotLine()
+						plotLine.lineColor = lineColor
+						plotLine.lineStyle = StrokeStyle(lineWidth: lineWidth, dash: lineStyles[lineStyle])
+						let shape = symbolShapes[pointStyle]
+						plotLine.pointShape = PointShape(shape.path, angle: shape.angle, fill: pointFill,
+														 color: pointColor, size: pointSize )
+						plotLine.legend = lineName
+						plotLine.secondary = useSecondary
+						plotLine.values = plotData.plotLines[i].values
+						
+						plotData.plotLines[i] = plotLine
+						plotData.scaleAxes()
+						dismiss()
+					}) { Text("Ok").foregroundColor(.accentColor).contentShape(Rectangle())}
+				}.buttonStyle(RoundedCorners(color: Color.primary.opacity(0.1), shadow: 2 ))
+			} // new end of List
+			.scrollDisabled(true)
+			.background(Color(.systemBackground))
+		}// \ScrollView
+		.frame(width: 300, height: 736)
 		.padding()
 		.onAppear {
 			i = plotData.settings.selection ?? 0
